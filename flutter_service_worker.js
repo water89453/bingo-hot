01492de7a571fe23ec46b481a512f610240b8f92
water89453 <water89453@users.noreply.github.com>
2025-08-18 16:43:01 +0000

    deploy: 4d60bbd51419f82c2fcbd0c1214f13cb1787a434

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "6ffd5e19865eac449dc7ba95a1afe273",
+const RESOURCES = {"flutter_bootstrap.js": "888558ce37a8cf3f8a5f67e8cd37a42c",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
